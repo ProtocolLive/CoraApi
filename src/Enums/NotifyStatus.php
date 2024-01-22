1d67acb3e45875e1903ee0a18317565a4be6f677
Protocol Corporation <protocollive@outlook.com>
2024-01-22 10:17:47 -0300

    Novo enum descoberto

--- src/Enums/NotifyStatus.php
+++ src/Enums/NotifyStatus.php
@@ -5,10 +5,11 @@
 namespace ProtocolLive\CoraApi\Enums;
 
 /**
- * @version 2024.01.18.00
+ * @version 2024.01.19.00
  */
 enum NotifyStatus:string{
   case Agendado = 'SCHEDULED';
   case Cancelado = 'CANCELED';
+  case Criado = 'CREATED';
   case Enviado = 'SENT';
 }
